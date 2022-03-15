9a29bd8f57dae488afa17ed097243877f9e021f7
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2022-03-15 23:23:29 +0800

    Add vuejs

---
+++ vuejs/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Todo from "./components/Todo.vue";
+</script>
+
+<template>
+  <main>
+    <Todo />
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
---
+++ vuejs/src/components/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo">{{ todo }}</li>
+    </ul>
+
+    <form v-on:submit.prevent="addTodo">
+      <input v-model="todoText" />
+      <button type="submit">Add Todo</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Todo",
+  data() {
+    return {
+      todos: [],
+      todoText: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      this.todos = [...this.todos, this.todoText];
+    },
+  },
+};
+</script>
+
+<style scoped></style>
