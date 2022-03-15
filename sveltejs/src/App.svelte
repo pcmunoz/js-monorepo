585fd5d2769e841cd6c40924f6e499d3cdf70414
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2022-03-16 02:09:06 +0800

    Add svelte

---
+++ sveltejs/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let todos = [];
+  let todoText = "";
+
+  function addTodo() {
+    todos = [...todos, todoText];
+  }
+</script>
+
+<main>
+  <ul>
+    {#each todos as todo}
+      <li>{todo}</li>
+    {/each}
+  </ul>
+  <form on:submit|preventDefault={addTodo}>
+    <input bind:value={todoText} />
+    <input type="submit" value="Add Todo" />
+  </form>
+</main>
+
+<style></style>
