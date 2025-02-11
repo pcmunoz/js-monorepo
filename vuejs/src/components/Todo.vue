4367ccc36950c02185bdcaa822fa32daf20d109e
Patrick Ceasar Muñoz <munozpatrickceasar@proton.me>
2025-02-12 04:26:03 +0800

    Update Vue

--- vuejs/src/components/Todo.vue
+++ vuejs/src/components/Todo.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
+  <div class="card flex justify-center">
+    <Button label="Verify" />
+  </div>
+  <div class="main">
     <ul>
-      <li v-for="todo in todos" v-bind:key="todo">{{ todo }}</li>
+      <li v-for="todo in todos" v-bind:key="todo">{{ todo.label }}</li>
     </ul>
-
+    <div>
+      <Listbox
+        v-model="selectedCity"
+        :options="cities"
+        optionLabel="name"
+        class="w-full md:w-56"
+      />
+    </div>
     <form v-on:submit.prevent="addTodo">
       <input v-model="todoText" />
       <button type="submit">Add Todo</button>
@@ -12,17 +22,33 @@
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
+
+const selectedCity = ref();
+const cities = ref([
+  { name: "New York", code: "NY" },
+  { name: "Rome", code: "RM" },
+  { name: "London", code: "LDN" },
+  { name: "Istanbul", code: "IST" },
+  { name: "Paris", code: "PRS" },
+]);
+
 export default {
   name: "Todo",
   data() {
     return {
+      selectedCity,
       todos: [],
       todoText: "",
+      cities,
     };
   },
   methods: {
     addTodo: function () {
-      this.todos = [...this.todos, this.todoText];
+      this.todos = [
+        ...this.todos,
+        { label: this.todoText, value: this.todoText },
+      ];
     },
   },
 };
--- vuejs/src/main.ts
+++ vuejs/src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
-createApp(App).mount("#app");
+import Button from "primevue/button";
+import { Listbox } from "primevue";
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+app.component("Button", Button);
+app.component("Listbox", Listbox);
+app.mount("#app");
